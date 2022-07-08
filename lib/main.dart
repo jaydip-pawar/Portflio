31070fe79e76d27ab7317662170e2618cb1a2e16
Jaydip Pawar <62420406+jaydip-pawar@users.noreply.github.com>
2022-07-08 20:39:27 +0530

    updated details

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       scrollBehavior: MaterialScrollBehavior().copyWith(
         dragDevices: {PointerDeviceKind.mouse, PointerDeviceKind.touch, PointerDeviceKind.stylus, PointerDeviceKind.unknown},
       ),
