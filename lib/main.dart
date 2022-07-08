f0ad153a63d0b09af37f778d9bdb59dc483b82d5
Jaydip Pawar <62420406+jaydip-pawar@users.noreply.github.com>
2022-07-08 20:32:52 +0530

    updated details

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:Portfolio/screens/home/home_screen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,6 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: MaterialScrollBehavior().copyWith(
+        dragDevices: {PointerDeviceKind.mouse, PointerDeviceKind.touch, PointerDeviceKind.stylus, PointerDeviceKind.unknown},
+      ),
       debugShowCheckedModeBanner: false,
       title: 'Jaydip Pawar',
       theme: ThemeData.dark().copyWith(
